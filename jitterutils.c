0cc074dd5cbaec971e967c5e76e1b9463dec195f
Daniel Wagner <daniel.wagner@siemens.com>
2018-10-25 17:49:29 +0200

    jitterutils: Add lockless single reader/writer ringbuffer
    The ringbuffer supports one reader and one writer without using
    locks. The reader thread only updates the reader index and the writer
    only the writer index. Furthermore, the read or write never blocks but
    we update a overflow counter to detect this situation.
    
    It is design is based on Andrew Morton's review on a ring
    buffer implementation:
    
    """
    A circular buffer implementation needs only head and tail
    indices. `size' above appears to be redundant.
    
    Implementation-wise, the head and tail indices should *not* be
    constrained to be less than the size of the buffer. They should be
    allowed to wrap all the way back to zero. This allows you to
    distinguish between the completely-empty and completely-full states
    while using 100% of the storage.
    """
    
    The compiler generates a wierd code for ringbuffer_read() do
    copy the data out of the ring buffer. We might need to change
    that in future but for the time being we can live with it even though
    it is not extremely fast.
    
    Link http://lkml.iu.edu/hypermail/linux/kernel/0409.1/2709.html
    Signed-off-by: Daniel Wagner <daniel.wagner@siemens.com>

--- jitterutils.c
+++ jitterutils.c
@@ -31,6 +31,86 @@
 
 #include "jitterdebugger.h"
 
+struct ringbuffer_sample {
+	struct timespec ts;
+	uint64_t val;
+};
+
+struct ringbuffer {
+	uint32_t size;
+	uint32_t overflow;
+	uint32_t read;
+	uint32_t write;
+	struct ringbuffer_sample *data;
+};
+
+static int ringbuffer_full(uint32_t size, uint32_t read, uint32_t write)
+{
+	return write - read == size;
+}
+
+static int ringbuffer_empty(uint32_t read, uint32_t write)
+{
+	return write == read;
+}
+
+static uint32_t ringbuffer_mask(uint32_t size, uint32_t idx)
+{
+	return idx & (size - 1);
+}
+
+struct ringbuffer *ringbuffer_create(unsigned int size)
+{
+	struct ringbuffer *rb;
+
+	/* Size needs to be of power of 2 */
+	if ((size & (size - 1)) != 0)
+		return NULL;
+
+	rb = calloc(1, sizeof(*rb));
+	if (!rb)
+		return NULL;
+
+	rb->size = size;
+	rb->data = calloc(rb->size, sizeof(struct ringbuffer_sample));
+
+	return rb;
+}
+
+int ringbuffer_write(struct ringbuffer *rb, struct timespec ts, uint64_t val)
+{
+	uint32_t read, idx;
+
+	read = READ_ONCE(rb->read);
+	if (ringbuffer_full(rb->size, read, rb->write)) {
+		rb->overflow++;
+		return 1;
+	}
+
+	idx = ringbuffer_mask(rb->size, rb->write + 1);
+	rb->data[idx].ts = ts;
+	rb->data[idx].val = val;
+
+	WRITE_ONCE(rb->write, rb->write + 1);
+	return 0;
+}
+
+int ringbuffer_read(struct ringbuffer *rb, struct timespec *ts, uint64_t *val)
+{
+	uint32_t write, idx;
+
+	write = READ_ONCE(rb->write);
+	if (ringbuffer_empty(rb->read, write))
+		return 1;
+
+	idx = ringbuffer_mask(rb->size, rb->read + 1);
+	*ts = rb->data[idx].ts;
+	*val = rb->data[idx].val;
+
+	WRITE_ONCE(rb->read, rb->read + 1);
+	return 0;
+}
+
 void _err_handler(int error, char *fmt, ...)
 {
 	va_list ap;
